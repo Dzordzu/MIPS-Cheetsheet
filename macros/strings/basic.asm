1fd4a017bfbf398f7f663ae1f207ff9baba96457
Tomasz Durda (Dzordzu) <tomekdur@wp.pl>
2019-05-26 11:53:10 +0000

    Added FIND macro

--- macros/strings/basic.asm
+++ macros/strings/basic.asm
@@ -1,3 +1,5 @@
+
+# v0 - length
 .macro LENGTH
 	addiu $sp, $sp, -4
 	sw $t0, 0($sp)
@@ -13,3 +15,31 @@ end:
 	lw $t0, 0($sp)
 	addiu $sp, $sp, 4
 .end_macro 
+
+# v0 - pointer
+# v1 - position (if v1 already had value, it would be added there)
+.macro FIND (%symbol) 
+	addiu $sp, $sp, -8
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+loop:
+	lb $t0, ($a0)
+	
+	seq $t1, $t0 $zero
+	bnez $t1, end
+	
+	
+	add $t1, $zero, %symbol
+	seq $t1, $t0, $t1
+	bnez $t1, end
+	
+	addiu $a0, $a0, 1
+	addiu $v1, $v1, 1
+	
+	j loop
+end:
+	move $v0, $a0
+	lw $t1, 4($sp)
+	lw $t0, 0($sp)
+	addiu $sp, $sp, 8
+.end_macro
